dbd65a380b19fecda3ee591171091f68a00d2280
farzana2407 <70813998+farzana2407@users.noreply.github.com>
2022-03-25 10:11:14 -0400

    Add files via upload

---
+++ webDriver.java
@@ -0,0 +1,22 @@
+package seleniumFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class webDriver {
+
+	public static void main(String[] args) {
+	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		
+		webmanager wm = new webmanager();
+		wm.
+
+	}
+
+}
